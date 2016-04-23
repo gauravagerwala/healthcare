5e685bad925a16b631b02d8527417a788cfe2766
Gaurav Agerwala <gauravagerwala@gmail.com>
2016-04-23 18:56:24 +0530

    Paitent registration completed

--- patient_reg.php
+++ patient_reg.php
@@ -1,16 +1,56 @@
 <?php
 	$dbhost = "localhost";
 	$dbuser = "root";
-	$dbpass = "snap_pnr";
+	$dbpass = "";
+	$db = "healthcare";
 
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
-	if($conn){
-		echo "Server running succesfully..";
+	if(mysqli_connect_errno()){
+		echo "Failed to connect to mysql : " . mysqli_connect_error();
+	} 
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$dob = $_POST["dob"];
+	$address = $_POST["address"];
+	$height = $_POST["height"];
+	$weight = $_POST['weight'];
+	$gender = $_POST["gender"];
+	if($_POST["password"] == $_POST["repassword"]){
+		$pass = $_POST["password"];
 	}else{
-		die "Server failed";
+		die("Passwords entered do not match");
 	}
-
+	 
 	
+	if($_POST["ec_name"] != null){
+		
+		$ec_name = $_POST["ec_name"];
+		$ec_phone = $_POST["ec_phone"];
+		$qContact = "INSERT INTO emergency_contact (ec_name, ec_phone) VALUES ('$ec_name','$ec_phone')";
+		
+		$a = mysqli_query($conn,$qContact);
+		if(!$a){
+			die(mysqli_error($conn));
+		}
+		$ec_id = mysqli_insert_id($conn);
 
+		
+		$qPatient = "INSERT INTO patient (p_name, p_email, p_phone, p_gender, p_dob, p_height, p_weight, p_password_hash, ec_id) 
+						   VALUES ('$name','$email','$phone','$gender','$dob','$height','$weight','$pass','$ec_id')";
+						   echo "$qPatient 1";
+	}else{
+		
+		$qPatient = "INSERT INTO patient (p_name, p_email, p_phone, p_gender, p_dob, p_height, p_weight, p_password_hash) 
+						   VALUES ('$name','$email','$phone','$gender','$dob','$height','$weight','$pass')";
+						   echo "$qPatient";	
+	}
+	
+	$a = mysqli_query($conn,$qPatient);
+	if(!$a){
+		die(mysqli_error($conn));
+	}
+	mysqli_close($conn);
 ?>
--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
-create table patient (p_id integer UNSIGNED NOT NULL AUTO_INCREMENT,p_name varchar(20) NOT NULL,p_email varchar(20) NOT NULL UNIQUE,p_phone varchar(10) NOT NULL,p_gender char(1),p_dob date NOT NULL,p_height float(5,2) NOT NULL,p_weight float(5,2) NOT NULL,p_password_hash varchar(40) NOT NULL,p_password_salt varchar(40),ec_id integer UNSIGNED NOT NULL,PRIMARY KEY(p_id));
+create table patient (p_id integer UNSIGNED NOT NULL AUTO_INCREMENT,p_name varchar(20) NOT NULL,p_email varchar(50) NOT NULL UNIQUE,p_phone varchar(15) NOT NULL,p_gender char(1),p_dob date,p_height integer,p_weight integer,p_password_hash varchar(40) NOT NULL,p_password_salt varchar(40),ec_id integer UNSIGNED,PRIMARY KEY(p_id));
 
 create table prescription(pres_id integer UNSIGNED NOT NULL AUTO_INCREMENT,diagnosis varchar(50) ,symptoms varchar(50),test_needed varchar(50) ,medication varchar(50) ,p_id integer UNSIGNED NOT NULL,PRIMARY KEY(pres_id));
 
-create table doctor(d_id integer UNSIGNED NOT NULL AUTO_INCREMENT,d_name varchar(20) NOT NULL,d_password_hash varchar(20) NOT NULL,d_email varchar(20) NOT NULL UNIQUE,d_phone varchar(10) ,d_address varchar(50) ,d_qualification varchar(20) NOT NULL,d_dob date ,PRIMARY KEY(d_id));
+create table doctor(d_id integer UNSIGNED NOT NULL AUTO_INCREMENT,d_name varchar(20) NOT NULL,d_password_hash varchar(20) NOT NULL,d_email varchar(50) UNIQUE,d_phone varchar(15) ,d_address varchar(50) ,d_qualification varchar(20) NOT NULL,d_dob date ,PRIMARY KEY(d_id));
 
-create table test(test_id integer NOT NULL AUTO_INCREMENT,test_name varchar(20) NOT NULL,test_result varchar(50) ,test_report varchar(50),p_id integer UNSIGNED NOT NULL,d_id integer UNSIGNED NOT NULL,pres_id integer UNSIGNED NOT NULL ,hs_id integer UNSIGNED NOT NULL,PRIMARY KEY(test_id));
+create table test(test_id integer NOT NULL AUTO_INCREMENT,test_name varchar(20) NOT NULL,test_result varchar(50) ,test_report varchar(50),p_id integer UNSIGNED NOT NULL,d_id integer UNSIGNED,pres_id integer UNSIGNED ,hs_id integer UNSIGNED NOT NULL,PRIMARY KEY(test_id));
 
-create table emergency_contact(ec_id integer UNSIGNED NOT NULL AUTO_INCREMENT,ec_name varchar(20) NOT NULL,ec_phone varchar(10),PRIMARY KEY(ec_id));
+create table emergency_contact(ec_id integer UNSIGNED NOT NULL AUTO_INCREMENT,ec_name varchar(20) NOT NULL,ec_phone varchar(15),PRIMARY KEY(ec_id));
 
-create table chamber(c_id integer UNSIGNED NOT NULL AUTO_INCREMENT,c_name varchar(20) NOT NULL,c_timing datetime ,c_address varchar(50) NOT NULL,c_phone varchar(10) ,d_id integer UNSIGNED NOT NULL,h_id integer UNSIGNED NOT NULL,PRIMARY KEY(c_id));
+create table chamber(c_id integer UNSIGNED NOT NULL AUTO_INCREMENT,c_name varchar(20) NOT NULL,c_timing datetime ,c_address varchar(50),c_phone varchar(15) ,d_id integer UNSIGNED NOT NULL,h_id integer UNSIGNED ,PRIMARY KEY(c_id));
 
 create table hospital(h_id integer UNSIGNED NOT NULL AUTO_INCREMENT,h_name varchar(20) NOT NULL,h_location varchar(50) ,PRIMARY KEY(h_id)); 
 
