3b9642b45b9019d7f97be945e74ff9398e75619b
Gaurav Agerwala <gauravagerwala@gmail.com>
2016-04-24 15:53:49 +0530

    Included patient session files

--- patient_reg.php
+++ patient_reg.php
@@ -1,15 +1,7 @@
 <?php
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$db = "healthcare";
-
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-
-	if(mysqli_connect_errno()){
-		echo "Failed to connect to mysql : " . mysqli_connect_error();
-	} 
-
+	
+	include("config.php");
+	
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$phone = $_POST["phone"];
@@ -23,7 +15,6 @@
 	}else{
 		die("Passwords entered do not match");
 	}
-	 
 	
 	if($_POST["ec_name"] != null){
 		
@@ -37,20 +28,17 @@
 		}
 		$ec_id = mysqli_insert_id($conn);
 
-		
 		$qPatient = "INSERT INTO patient (p_name, p_email, p_phone, p_gender, p_dob, p_height, p_weight, p_password_hash, ec_id) 
-						   VALUES ('$name','$email','$phone','$gender','$dob','$height','$weight','$pass','$ec_id')";
-						   echo "$qPatient 1";
+						   VALUES ('$name','$email','$phone','$gender','$dob','$height','$weight','$pass','$ec_id')";						   
 	}else{
-		
 		$qPatient = "INSERT INTO patient (p_name, p_email, p_phone, p_gender, p_dob, p_height, p_weight, p_password_hash) 
 						   VALUES ('$name','$email','$phone','$gender','$dob','$height','$weight','$pass')";
-						   echo "$qPatient";	
 	}
 	
 	$a = mysqli_query($conn,$qPatient);
 	if(!$a){
 		die(mysqli_error($conn));
 	}
+	
 	mysqli_close($conn);
 ?>
